9e1629e0d46dac837b7ae7f3640ae1d31935df73
W.Y.Park <wypark70@gmail.com>
2025-05-12 06:57:21 +0900

    log 컴포넌트 이름 변경경

--- svelte-app/src/routes/server/+page.svelte
+++ svelte-app/src/routes/server/+page.svelte
@@ -1,10 +1,10 @@
 <script>
 	// 필요한 스크립트 작성
-	import SmtpLogs from '$lib/components/SmtpLogs.svelte';
+	import MailManagerServerLogs from '$lib/components/MailManagerServerLogs.svelte';
 </script>
 
 <div class="container mx-auto px-4 py-8">
 	<div class="mb-8">
-		<SmtpLogs />
+		<MailManagerServerLogs />
 	</div>
 </div>
